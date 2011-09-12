0cbeae3e1535f917b78f5ee42ee8f58482414028
Kory Becker <kbecker@primaryobjects.com>
2011-09-12 11:17:36 -0400

    Updated assembly properties.

--- PasteBinTest/Properties/AssemblyInfo.cs
+++ PasteBinTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PasteBinTest")]
+[assembly: AssemblyTitle("PasteBinSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("PasteBinTest")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PasteBinSample")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
